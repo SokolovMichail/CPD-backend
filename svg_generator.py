41250a50a1b6466f451010a1a4d5f461643497b1
Sokolov <sokolovmisha44@gmail.com>
2020-12-21 14:04:49 +0300

    Refactored for compliance with new features.

--- svg_generator.py
+++ svg_generator.py
@@ -11,9 +11,14 @@ class SVG_Generator:
             os.mkdir(SVG_CATALOGUE_PATH)
 
     def generate_svg(self, token,graph):
-        filename =  os.path.join(SVG_CATALOGUE_PATH,token+'.svg')
+        filename =  os.path.join(SVG_CATALOGUE_PATH,token)
         graph_new = re.sub('dpi=\d*','dpi=0',graph)
         s = Source(graph_new, filename=filename, format="svg")
-        s.save()
-        return os.path.abspath(filename)
+        s.render(filename=filename, format="svg")
+        os.remove(filename)
+        res = ''
+        with open(filename+'.svg','r') as f:
+            res = f.read()
+        os.remove(filename+'.svg')
+        return res
 
